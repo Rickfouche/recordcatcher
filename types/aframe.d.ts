cca796787c3b5f2a414ef849af8f2bc891433a16
Rick Fouche <Rickfouche@gmail.com>
2025-09-11 18:03:52 -0400

    your message

---
+++ types/aframe.d.ts
@@ -0,0 +1,10 @@
+// types/aframe.d.ts
+declare namespace JSX {
+  interface IntrinsicElements {
+    'a-scene': any;
+    'a-entity': any;
+    'a-assets': any;
+    'a-plane': any;
+    'a-box': any;
+  }
+}
