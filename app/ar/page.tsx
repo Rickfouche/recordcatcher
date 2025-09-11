2fd19012ca449a5faa935515daa8d7932b34baf8
Rick Fouche <Rickfouche@gmail.com>
2025-09-11 17:25:06 -0400

    your message

---
+++ app/ar/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Script from 'next/script';
+import { supabase } from '@/lib/supabaseClient';
+
+/* … rest of the React code … */
